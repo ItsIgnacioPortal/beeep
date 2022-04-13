91baf01e5c85b8413510039ab0e77894d8ae4a35
PinkDev1 <5990@protonmail.com>
2022-04-13 05:17:35 -0300

    Removed everything that isn't for windows

--- alert_unsupported.go
+++
@@ -1,9 +0,0 @@
-
-
-
-package main
-
-
-func Alert(title, message, appIcon string) error {
-	return ErrUnsupported
-}
--- notify_unsupported.go
+++
@@ -1,9 +0,0 @@
-
-
-
-package main
-
-
-func Notify(title, message string) error {
-	return ErrUnsupported
-}
